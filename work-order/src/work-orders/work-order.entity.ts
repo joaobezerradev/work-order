410fe0ccec491e0f37b9282e60495c9a636ca846
Joao Bezerra <vitor.lira98@gmail.com>
2021-05-27 20:17:23 -0300

    feat: add relations in work-order entity

--- work-order/src/work-orders/work-order.entity.ts
+++ work-order/src/work-orders/work-order.entity.ts
@@ -1,15 +1,24 @@
-import { Column, Entity, JoinColumn, ManyToOne } from 'typeorm';
+import { CommentEnity } from 'src/comments/comment.entity';
+import { getUTCDate } from 'src/commons/functions/get-utc-date.function';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  OneToMany,
+} from 'typeorm';
 import { BaseEntity } from '../commons/entities/abstract.entity';
 import { UserEntity } from '../users/user.entity';
 import { WorkOrderStatus } from './enums/work-order-status.enum';
 
 @Entity('workorder')
 export class WorkOrderEntity extends BaseEntity {
-  @Column('uuid')
+  @Column('uuid', { name: 'userid' })
   userId: string;
 
   @ManyToOne(() => UserEntity)
-  @JoinColumn({ name: 'userId' })
+  @JoinColumn({ name: 'userid' })
   user: UserEntity;
 
   @Column()
@@ -21,9 +30,16 @@ export class WorkOrderEntity extends BaseEntity {
   @Column()
   status: WorkOrderStatus;
 
-  @Column({ type: Date })
-  startDate = new Date();
+  @OneToMany(() => CommentEnity, (comment: CommentEnity) => comment.workOrder)
+  comments: CommentEnity[];
 
-  @Column({ type: Date })
+  @CreateDateColumn({ name: 'startdate', type: 'timestamp with time zone' })
+  startDate = getUTCDate();
+
+  @CreateDateColumn({
+    name: 'enddate',
+    type: 'timestamp with time zone',
+    nullable: true,
+  })
   endDate: Date;
 }
