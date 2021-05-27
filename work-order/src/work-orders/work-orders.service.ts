658878c0eb0d7b0e399e1114d91c51f430ec5e15
Joao Bezerra <vitor.lira98@gmail.com>
2021-05-27 18:47:36 -0300

    feat: create service create work order

--- work-order/src/work-orders/work-orders.service.ts
+++ work-order/src/work-orders/work-orders.service.ts
@@ -1,4 +1,38 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UsersService } from 'src/users/users.service';
+import { CreateWorkOrderDto } from './dtos/create-work-order.dto';
+import { WorkOrderEntity } from './work-order.entity';
+import { WorkOrderRepository } from './work-order.repository';
 
 @Injectable()
-export class WorkOrdersService {}
+export class WorkOrdersService {
+  constructor(
+    @InjectRepository(WorkOrderRepository)
+    private workOrderRepository: WorkOrderRepository,
+    private usersService: UsersService,
+  ) {}
+
+  async createWorkOrder(
+    createWorkOrderDto: CreateWorkOrderDto,
+  ): Promise<WorkOrderEntity> {
+    const { userId, description, price } = createWorkOrderDto;
+
+    const foundUser = await this.usersService.findById(userId);
+
+    if (!foundUser) {
+      throw new NotFoundException();
+    }
+
+    const newWorkOrder = this.workOrderRepository.create({
+      userId,
+      description,
+      price,
+      endDate: null,
+    });
+
+    await newWorkOrder.save();
+
+    return newWorkOrder;
+  }
+}
