12dfb9172277760dc4ac157a3840447be523c7ce
Joao Bezerra <vitor.lira98@gmail.com>
2021-05-27 20:11:05 -0300

    chore: create comments module

--- work-order/src/app.module.ts
+++ work-order/src/app.module.ts
@@ -5,6 +5,7 @@ import typeOrmConfig from './config/typeorm.config';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { WorkOrdersModule } from './work-orders/work-orders.module';
+import { CommentsModule } from './comments/comments.module';
 
 @Module({
   imports: [
@@ -17,6 +18,7 @@ import { WorkOrdersModule } from './work-orders/work-orders.module';
     UsersModule,
     AuthModule,
     WorkOrdersModule,
+    CommentsModule,
   ],
 })
 export class AppModule {}
