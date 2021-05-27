15ef141c6e95fd28197854e1e8e53b20dcbb5bbd
Joao Bezerra <vitor.lira98@gmail.com>
2021-05-27 18:46:44 -0300

    feat: create dto work order response

---
+++ work-order/src/work-orders/dtos/work-order-response.dto.ts
@@ -0,0 +1,32 @@
+import { classToClass, Expose, plainToClass } from 'class-transformer';
+import { BaseDto } from 'src/commons/dtos/base.dto';
+import { WorkOrderStatusEnum } from '../enums/work-order-status.enum';
+import { WorkOrderEntity } from '../work-order.entity';
+
+export class WorkOrderResponseDto extends BaseDto {
+  @Expose()
+  userId: string;
+
+  @Expose()
+  description: string;
+
+  @Expose()
+  price: number;
+
+  @Expose()
+  status: WorkOrderStatusEnum;
+
+  @Expose()
+  startDate: Date;
+
+  @Expose()
+  endDate: Date | null;
+
+  static factory(workOrder: WorkOrderEntity): WorkOrderResponseDto {
+    const responseData = plainToClass(this, workOrder, {
+      ignoreDecorators: true,
+    });
+
+    return classToClass(responseData, { excludeExtraneousValues: true });
+  }
+}
