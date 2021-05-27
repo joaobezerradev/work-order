09c31a3e0bcf2f3ecea14a416d73c464a8160ca1
Joao Bezerra <vitor.lira98@gmail.com>
2021-05-27 18:45:58 -0300

    feat: create dto create work order

---
+++ work-order/src/work-orders/dtos/create-work-order.dto.ts
@@ -0,0 +1,28 @@
+import { ApiProperty } from '@nestjs/swagger';
+import {
+  IsNotEmpty,
+  IsNumber,
+  IsString,
+  IsUUID,
+  MinLength,
+} from 'class-validator';
+
+export class CreateWorkOrderDto {
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(5)
+  @ApiProperty({ type: String, example: 'Lorem ipsum et' })
+  description: string;
+
+  @IsUUID('4')
+  @ApiProperty({
+    type: String,
+    example: 'b985238f-1db1-476b-8f85-0a0f68707e8f',
+  })
+  userId: string;
+
+  @IsNotEmpty()
+  @IsNumber()
+  @ApiProperty({ type: Number, example: '4.000' })
+  price: number;
+}
