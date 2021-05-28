edf051fddd242e359c353173fd8c9b58fd326554
Joao Bezerra <vitor.lira98@gmail.com>
2021-05-28 00:33:43 -0300

    feat: add optional property

--- work-order/src/work-orders/dtos/get-work-order-filter.dto.ts
+++ work-order/src/work-orders/dtos/get-work-order-filter.dto.ts
@@ -1,12 +1,15 @@
+import { ApiPropertyOptional } from '@nestjs/swagger';
 import { IsEnum, IsOptional, IsString } from 'class-validator';
 import { WorkOrderStatus } from '../enums/work-order-status.enum';
 
 export class GetWorkOrderFilterDto {
   @IsOptional()
   @IsEnum(WorkOrderStatus)
+  @ApiPropertyOptional()
   status?: WorkOrderStatus;
 
   @IsOptional()
   @IsString()
+  @ApiPropertyOptional()
   search?: string;
 }
