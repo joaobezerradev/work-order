c0c9600146605596e9ce24a1f1c499b991263131
Joao Bezerra <vitor.lira98@gmail.com>
2021-05-27 23:41:12 -0300

    feat: impor repository in module

--- work-order/src/work-orders/work-orders.module.ts
+++ work-order/src/work-orders/work-orders.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from 'src/users/users.module';
+import { WorkOrderRepository } from './work-order.repository';
 import { WorkOrdersController } from './work-orders.controller';
 import { WorkOrdersService } from './work-orders.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([WorkOrderRepository]), UsersModule],
   controllers: [WorkOrdersController],
   providers: [WorkOrdersService],
   exports: [WorkOrdersService],
