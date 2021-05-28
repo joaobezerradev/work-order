afa11463c2dca3e5c3f0be37b39be92cd8b5eee7
Joao Bezerra <vitor.lira98@gmail.com>
2021-05-28 00:35:31 -0300

    feat: create service comments

--- work-order/src/comments/comments.service.ts
+++ work-order/src/comments/comments.service.ts
@@ -1,4 +1,53 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { WorkOrdersService } from 'src/work-orders/work-orders.service';
+import { CommentEnity } from './comment.entity';
+import { CommentRepository } from './comment.repository';
+import { CreateCommentDto } from './dtos/create-comment.dto';
 
 @Injectable()
-export class CommentsService {}
+export class CommentsService {
+  constructor(
+    private commentRepository: CommentRepository,
+    private workOrdersService: WorkOrdersService,
+  ) {}
+
+  async getCommentsByWorkOrderId(workOrderId: string): Promise<CommentEnity[]> {
+    const foundWorkOrder = await this.workOrdersService.getWorkOrderById(
+      workOrderId,
+    );
+
+    if (!foundWorkOrder) {
+      throw new NotFoundException();
+    }
+
+    const comments = await this.commentRepository.find({
+      where: { workOrderId },
+      relations: ['workOrder'],
+    });
+
+    return comments;
+  }
+
+  async createComment(
+    createCommentDto: CreateCommentDto,
+  ): Promise<CommentEnity> {
+    const { workOrderId, description } = createCommentDto;
+
+    const foundWorkOrder = await this.workOrdersService.getWorkOrderById(
+      workOrderId,
+    );
+
+    if (!foundWorkOrder) {
+      throw new NotFoundException();
+    }
+
+    const newComment = this.commentRepository.create({
+      workOrderId,
+      description,
+    });
+
+    await newComment.save();
+
+    return newComment;
+  }
+}
