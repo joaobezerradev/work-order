40f34fdb373f7a6a88528f1c15d4293b435eccb1
Joao Bezerra <vitor.lira98@gmail.com>
2021-05-27 23:39:30 -0300

    feat: import comment repository in module

--- work-order/src/comments/comments.module.ts
+++ work-order/src/comments/comments.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { CommentsService } from './comments.service';
 import { CommentsController } from './comments.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CommentRepository } from './comment.repository';
+import { WorkOrdersModule } from 'src/work-orders/work-orders.module';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([CommentRepository]), WorkOrdersModule],
   providers: [CommentsService],
   controllers: [CommentsController],
   exports: [CommentsService],
